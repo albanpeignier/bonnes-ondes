7273a7122faa97b3b397472a97eae1ef550b1f63
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-07-22 18:27:18 +0000

    Changement de toutes les routes publiques. Refs #3
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1219 20e14386-5fff-0310-853f-b84fc43df78c

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,15 @@ ActionController::Routing::Routes.draw do |map|
   map.connect 'compte/activate/:code', :controller => 'account', :action => 'activate'
   map.connect 'compte/:action', :controller => 'account'
 
-  map.connect '/emission/:show_slug', :controller => 'public', :action => 'show'
-  map.connect '/emission/:show_slug/episode/:episode_slug', :controller => 'public', :action => 'episode'
-  map.connect '/emission/:show_slug/episode/:episode_slug/ecoute/:content_slug', :controller => 'public', :action => 'content'
-  map.connect '/emission/:show_slug/episode/:episode_slug/ecouter/:content_slug', :controller => 'public', :action => 'playlist'
-  map.connect '/emission/:show_slug/feed', :controller => 'public', :action => 'feed'
+  map.connect 'ep/:episode_slug', :controller => 'public', :action => 'episode'
+  map.connect 'ep/:episode_slug/ecoute/:content_slug', :controller => 'public', :action => 'content'
+  map.connect 'ep/:episode_slug/ecouter/:content_slug', :controller => 'public', :action => 'playlist'
+  map.connect 'feed', :controller => 'public', :action => 'feed'
 
   # You can have the root of your site routed by hooking up ''
   # -- just remember to delete public/index.html.
   map.connect '', :controller => "public", :action => "welcome"
+  map.connect '', :controller => "public", :action => "show"
 
   map.connect '/:action', :controller => "public"
 
