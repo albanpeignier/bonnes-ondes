25a022623b3250b56d9d970cc4a236ae68fa01cc
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-07-25 14:02:53 +0000

    Mise en place du filtre liquid prettydate
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1247 20e14386-5fff-0310-853f-b84fc43df78c

--- config/initializers/liquid_filters.rb
+++ config/initializers/liquid_filters.rb
@@ -6,6 +6,37 @@ end
 
 Liquid::Template.register_filter(TextFilter)
 
+module PrettyDateFilter
+
+  def prettydate(input, format = nil)
+      date = case input
+      when String
+        Time.parse(input)
+      when Date, Time, DateTime
+        input
+      else
+        return input
+      end
+
+      if format.blank?
+        if date.today?
+          format = "aujourd'hui à %Hh%M"
+        end
+        if date.yesterday?
+          format = 'hier à %Hh%M'
+        end
+      end
+
+      date.strftime(format.to_s)
+    rescue => e
+      input
+    end
+
+
+end
+
+Liquid::Template.register_filter(PrettyDateFilter)
+
 class Liquid::Drop
 
   protected
