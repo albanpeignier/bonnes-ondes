e79c722e7f4d0cbdd7258fc7c35dcf3f470845e5
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-07-27 16:01:23 +0000

    Fixe l'affichage des dates de prettydate
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1264 20e14386-5fff-0310-853f-b84fc43df78c

--- config/initializers/date_in_french.rb
+++ config/initializers/date_in_french.rb
@@ -2,6 +2,7 @@
 require 'date'
 
 silence_warnings do
+  Date.const_set "MONTHNAMES_EN", Date::MONTHNAMES
   Date.const_set "MONTHNAMES", [nil] + %w(Janvier Février Mars Avril Mai Juin Juillet Août Septembre Octobre Novembre Décembre)
   Date.const_set "DAYNAMES", %w(Dimanche Lundi Mardi Mercredi Jeudi Vendredi Samedi)
   Date.const_set "ABBR_MONTHNAMES", [nil] + %w(Jan Fév Mar Avr Mai Juin Juil Aoû Sep Oct Nov Dec)
@@ -13,3 +14,19 @@ ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :time => '%Hh%M' # 18h11
 )
+
+class Time
+
+  def strftime_local(format)
+    formatted = strftime(format)
+
+    if format.include? '%B'
+      Date::MONTHNAMES_EN.each_with_index do |month_en, index|
+        formatted = formatted.gsub(month_en, Date::MONTHNAMES[index]) unless month_en.nil?
+      end
+    end
+
+    formatted
+  end
+
+end
--- config/initializers/liquid_filters.rb
+++ config/initializers/liquid_filters.rb
@@ -9,28 +9,30 @@ Liquid::Template.register_filter(TextFilter)
 module PrettyDateFilter
 
   def prettydate(input, format = nil)
-      date = case input
+    date = case input
       when String
         Time.parse(input)
       when Date, Time, DateTime
         input
       else
         return input
-      end
-
-      if format.blank?
-        if date.today?
-          format = "aujourd'hui à %Hh%M"
-        end
-        if date.yesterday?
-          format = 'hier à %Hh%M'
-        end
-      end
-
-      date.strftime(format.to_s)
-    rescue => e
-      input
     end
+
+    if format.blank?
+      if date.today?
+        format = 'aujourd\'hui à %Hh%M'
+      elsif date.yesterday?
+        format = 'hier à %Hh%M'
+      else
+        format = 'le %d %B %Y à %Hh%M'
+      end
+    end
+
+    # use a fixed strftime (with french month)
+    date.strftime_local(format.to_s)
+  rescue => e
+    input
+  end
 
 
 end
