8952a8d15724a07fb02fa4123ed7ee6f59e3744f
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-07-28 14:44:18 +0000

    Template pour les javascripts et message particulier quand pas de contenu
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1275 20e14386-5fff-0310-853f-b84fc43df78c

---
+++ templates/re2008/_javascripts.js
@@ -0,0 +1,2 @@
+<script src="{{theme.url_for_assets}}/js/prototype.min.js" type="text/javascript"></script>
+<script src="{{theme.url_for_assets}}/js/effects.min.js" type="text/javascript"></script>
