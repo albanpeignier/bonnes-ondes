229ce6b39036088e8e95057b2abefead20d8f71c
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-07-29 18:19:41 +0000

    Reduit les double -- dans les slugs
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1289 20e14386-5fff-0310-853f-b84fc43df78c

--- lib/slug.rb
+++ lib/slug.rb
@@ -1,12 +1,20 @@
 class Slug
   def self.slugify(text)
     return "" if text.nil? or text.strip.blank?
-    replace_specials(text.downcase).gsub(/[^a-z0-9-]+/, ' ').strip.gsub(' ','-')[0..29]
+    reduce_separators(replace_specials(text.downcase).gsub(/[^a-z0-9-]+/, ' ').strip.gsub(' ','-')[0..29])
   end
-  
+
   private
+
+  def self.reduce_separators(text)
+    while text.include? '--'
+      text = text.gsub('--','-')
+    end
+    text
+  end
+
   def self.replace_specials(text)
     text.gsub(/[éêè]/,"e").gsub("à","a").gsub("ù","u").gsub("ç","c")
   end
-  
+
 end
