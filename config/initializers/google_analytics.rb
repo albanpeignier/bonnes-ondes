b144cf42e69d38b178690cc6e01289ed0e8259af
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-08-09 09:21:35 +0000

    Define default Google Analytics account. Refs #18
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1337 20e14386-5fff-0310-853f-b84fc43df78c

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,3 +1,2 @@
-Rubaidh::GoogleAnalytics.tracker_id = 'UA-1896598-5'
+Rubaidh::GoogleAnalytics.default_account = Rubaidh::GoogleAnalytics.new('UA-1896598-5')
 Rubaidh::GoogleAnalytics.environments = ['production']
-Rubaidh::GoogleAnalytics.domain_name = 'bonnes-ondes.fr'
