f23c347ed9133ae8307201a6ec65d15715d97006
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2008-08-09 09:21:33 +0000

    Multiple Google Analytics support. Refs #18
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1336 20e14386-5fff-0310-853f-b84fc43df78c

--- vendor/plugins/google_analytics/lib/rubaidh/google_analytics.rb
+++ vendor/plugins/google_analytics/lib/rubaidh/google_analytics.rb
@@ -1,15 +1,22 @@
 module Rubaidh # :nodoc:
   module GoogleAnalyticsMixin
     def google_analytics_code
-      GoogleAnalytics.google_analytics_code(request.ssl?) if GoogleAnalytics.enabled?(request.format)
+      GoogleAnalytics.google_analytics_code(request) if GoogleAnalytics.enabled?(request.format)
     end
-    
+
     # An after_filter to automatically add the analytics code.
-    # Add the code at the top of the page to support calls to _trackPageView 
+    # Add the code at the top of the page to support calls to _trackPageView
     #   (see http://www.google.com/support/googleanalytics/bin/answer.py?answer=55527&topic=11006)
     def add_google_analytics_code
       response.body.sub! '</body>', "#{google_analytics_code}</body>" if response.body.respond_to?(:sub!)
     end
+  end
+
+  module GoogleAnalyticsAccountSupport
+
+    @google_analytics_account = nil
+    attr_accessor :google_analytics_account
+
   end
 
   class GoogleAnalyticsConfigurationError < StandardError; end
@@ -17,20 +24,25 @@ module Rubaidh # :nodoc:
   class GoogleAnalytics
     # Specify the Google Analytics ID for this web site.  This can be found
     # as the value of +_uacct+ in the Javascript excerpt
-    @@tracker_id = nil
-    cattr_accessor :tracker_id
+    @tracker_id = nil
+    attr_accessor :tracker_id
 
     # Specify a different domain name from the default.  You'll want to use
     # this if you have several subdomains that you want to combine into
     # one report.  See the Google Analytics documentation for more
     # information.
-    @@domain_name = nil
-    cattr_accessor :domain_name
+    @domain_name = nil
+    attr_accessor :domain_name
+
+    def initialize(tracker_id, domain_name = nil)
+      @tracker_id = tracker_id
+      @domain_name = domain_name
+    end
 
     # Specify whether the legacy Google Analytics code should be used.
     @@legacy_mode = false
     cattr_accessor :legacy_mode
-    
+
     # I can't see why you'd want to do this, but you can always change the
     # analytics URL.  This is only applicable in legacy mode.
     @@analytics_url = 'http://www.google-analytics.com/urchin.js'
@@ -45,22 +57,46 @@ module Rubaidh # :nodoc:
     # to 'production' only.
     @@environments = ['production']
     cattr_accessor :environments
-    
+
     # The formats for which to add.  Defaults
     # to :html only.
     @@formats = [:html]
     cattr_accessor :formats
 
+    @@default_account = nil
+    cattr_accessor :default_account
+
+    @@request_accounts = []
+    cattr_accessor :request_accounts
+
+    def self.add_request_account(account)
+      @@request_accounts << account
+    end
+
     # Return true if the Google Analytics system is enabled and configured
     # correctly for the specified format
     def self.enabled?(format)
-      raise Rubaidh::GoogleAnalyticsConfigurationError if tracker_id.blank? || analytics_url.blank?
+      raise Rubaidh::GoogleAnalyticsConfigurationError if default_account.blank? || analytics_url.blank?
       environments.include?(RAILS_ENV) && formats.include?(format.to_sym)
     end
-    
-    def self.google_analytics_code(ssl = false)
-      return legacy_google_analytics_code(ssl) if legacy_mode
 
+    def self.accounts(request)
+      [ default_account, request.google_analytics_account ].compact
+    end
+
+    def self.collect_accounts_code(request, legacy = false)
+      method = legacy ? :legacy_google_analytics_code : :google_analytics_code
+
+      accounts(request).collect do |account|
+        account.send method, request.ssl?
+      end.join("\n")
+    end
+
+    def self.google_analytics_code(request)
+      collect_accounts_code(request, legacy_mode)
+    end
+
+    def google_analytics_code(ssl = false)
       extra_code = domain_name.blank? ? nil : "pageTracker._setDomainName(\"#{domain_name}\");"
 
       code = <<-HTML
@@ -80,7 +116,11 @@ module Rubaidh # :nodoc:
     end
 
     # Run the legacy version of the Google Analytics code.
-    def self.legacy_google_analytics_code(ssl = false)
+    def self.legacy_google_analytics_code(request)
+      collect_accounts_code(request, false)
+    end
+
+    def legacy_google_analytics_code(ssl = false)
       extra_code = domain_name.blank? ? nil : "_udn = \"#{domain_name}\";"
       url = ssl ? analytics_ssl_url : analytics_url
 
