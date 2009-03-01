32598ba902c8d3361fa93393be9b9386d39ef404
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2009-03-01 12:15:29 +0000

    Disable content play when unavailable in re2008
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1373 20e14386-5fff-0310-853f-b84fc43df78c

--- spec/templates/re2008/content.liquid_spec.rb
+++ spec/templates/re2008/content.liquid_spec.rb
@@ -22,7 +22,6 @@ describe "re2008/content" do
     render :layout => false, :template => "#{@template.slug}/#{view}",
       :locals => { view.to_sym => object }
   end
-
   
   it "should have title 'Esperanzah! 2008 - Radio - Ecouter content_episode_title - content_name'" do
     @content.name = 'content_name'
@@ -32,10 +31,26 @@ describe "re2008/content" do
     response.should have_tag('title', 'Esperanzah! 2008 - Radio - Ecouter content_episode_title - content_name')
   end
 
-  it "should display content embedded player" do
+  it "should display embedded player when content is available" do
+    @content_liquid.stub!(:available?).and_return(true)
+
     @content_liquid.should_receive(:embedded_player).and_return("<embed id='test'/>")
     render_template :content, @content
     response.should have_tag('embed[id=test]')
+  end
+
+  it "should not display embedded player when content isn't available" do
+    @content_liquid.stub!(:available?).and_return(false)
+
+    @content_liquid.should_not_receive(:embedded_player)
+    render_template :content, @content
+    response.should_not have_tag('embed')
+  end
+
+  it "should display 'Contenu non disponible' when content isn't available" do
+    @content_liquid.stub!(:available?).and_return(false)
+    render_template :content, @content
+    response.should have_tag('p',/ce contenu n'est plus disponible/i)
   end
 
 end
