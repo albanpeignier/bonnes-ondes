f87a5efd2334caaa8455cef13a74401abe0433fc
alban <alban@20e14386-5fff-0310-853f-b84fc43df78c>
2009-03-01 12:15:22 +0000

    Disable enclosure in public/feed when content is unavailable
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1370 20e14386-5fff-0310-853f-b84fc43df78c

--- spec/views/public/_feed_content.rxml_spec.rb
+++ spec/views/public/_feed_content.rxml_spec.rb
@@ -12,11 +12,21 @@ describe "Partial public/feed_content" do
     render :partial => "public/feed_content", :object => @content
   end
 
-  it "should display enclosure with content_url" do
+  it "should display enclosure with content url if content is available" do
+    @content.stub!(:available?).and_return(true)
+
     content_url = "http://dummy"
     @content.should_receive(:content_url).with(:format => :mp3).and_return(content_url)
     render_partial
     response.should have_tag("enclosure[url=?][type=audio/mpeg]", content_url)
+  end
+
+  it "should not display enclosure if content isn't available" do
+    @content.stub!(:available?).and_return(false)
+
+    @content.should_not_receive(:content_url)
+    render_partial
+    response.should_not have_tag("enclosure")
   end
 
 end
