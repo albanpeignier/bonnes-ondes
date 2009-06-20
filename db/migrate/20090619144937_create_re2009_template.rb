20e42507e6555b491e3d9fc5f611a76fe06c489b
tim <tim@20e14386-5fff-0310-853f-b84fc43df78c>
2009-06-20 16:58:41 +0000

    adding a basic templates for re2009
    git-svn-id: svn+ssh://svn.tryphon.org/kolaka/bonnes-ondes/trunk@1374 20e14386-5fff-0310-853f-b84fc43df78c

---
+++ db/migrate/20090619144937_create_re2009_template.rb
@@ -0,0 +1,9 @@
+class CreateRe2009Template < ActiveRecord::Migration
+  def self.up
+    Template.create :name => "Esperanzah! 2009", :slug => "re2009"
+  end
+
+  def self.down
+    Template.find_by_slug('re2009').destroy
+  end
+end
