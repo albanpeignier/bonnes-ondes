6a7bb1caf647085f8109d3b3627715486d97b81f
Alban Peignier <alban.peignier@free.fr>
2009-06-27 11:38:47 +0200

    Fix an horrible rewrite in migration of re2009

--- db/migrate/20090619144937_create_re2009_template.rb
+++ db/migrate/20090619144937_create_re2009_template.rb
@@ -1,9 +1,9 @@
 class CreateRe2009Template < ActiveRecord::Migration
   def self.up
-    Template.create :name => "Radio RMLL 2009", :slug => "rmll2009"
+    Template.create :name => "Radio Esperanzah! 2009", :slug => "re2009"
   end
 
   def self.down
-    Template.find_by_slug('rmll2009').destroy
+    Template.find_by_slug('re2009').destroy
   end
 end
