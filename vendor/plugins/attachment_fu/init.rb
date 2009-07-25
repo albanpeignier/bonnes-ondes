d69572e8ba25de572a2694801e9992550c3e8834
Alban Peignier <alban.peignier@free.fr>
2009-07-25 20:33:46 +0200

    Only log a message when Technoweenie::AttachmentFu.tempfile_path can't be created

--- vendor/plugins/attachment_fu/init.rb
+++ vendor/plugins/attachment_fu/init.rb
@@ -11,6 +11,11 @@ end
 require 'geometry'
 ActiveRecord::Base.send(:extend, Technoweenie::AttachmentFu::ActMethods)
 Technoweenie::AttachmentFu.tempfile_path = ATTACHMENT_FU_TEMPFILE_PATH if Object.const_defined?(:ATTACHMENT_FU_TEMPFILE_PATH)
-FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
+
+begin
+  FileUtils.mkdir_p Technoweenie::AttachmentFu.tempfile_path
+rescue => e
+  $stderr.puts "can't create #{Technoweenie::AttachmentFu.tempfile_path}"
+end
 
 $:.unshift(File.dirname(__FILE__) + '/vendor')
