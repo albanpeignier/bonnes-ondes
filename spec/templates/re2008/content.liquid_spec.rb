2993dd6b73a9d3f53d894314219935cde8a61f48
Alban Peignier <alban.peignier@free.fr>
2009-08-02 16:21:52 +0200

    Load episodes fixtures when required in specs

--- spec/templates/re2008/content.liquid_spec.rb
+++ spec/templates/re2008/content.liquid_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "re2008/content" do
 
-  fixtures :contents
+  fixtures :contents, :episodes
 
   before do
     @template = Template.new :slug => 're2008'
