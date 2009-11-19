a8013756a330395302d183cdd09e81cdf0c4d462
Alban Peignier <alban.peignier@free.fr>
2009-11-19 07:48:34 +0100

    Use domain 'none' to track domain names with Google Analytics

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,2 +1,6 @@
-Rubaidh::GoogleAnalytics.default_account = Rubaidh::GoogleAnalytics.new('UA-1896598-5')
+Rubaidh::GoogleAnalytics.default_account = Rubaidh::GoogleAnalytics.new('UA-1896598-5').tap do |account|
+  # Use domain "none" to track all domains managed by bonnes-ondes
+  # http://www.google.com/support/analytics/bin/answer.py?answer=55503
+  account.domain_name = "none"
+end
 Rubaidh::GoogleAnalytics.environments = ['production']
