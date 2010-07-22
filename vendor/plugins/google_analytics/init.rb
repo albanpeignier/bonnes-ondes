e006ed902021ef214598dcc7912d379f50671a30
Alban Peignier <alban@tryphon.eu>
2010-07-22 18:24:12 +0200

    Include Rubaidh::GoogleAnalyticsAccountSupport in ActionController::Request

--- vendor/plugins/google_analytics/init.rb
+++ vendor/plugins/google_analytics/init.rb
@@ -1,7 +1,8 @@
 require 'rubaidh/google_analytics'
 require 'rubaidh/view_helpers'
+
 ActionController::Base.send :include, Rubaidh::GoogleAnalyticsMixin
 ActionController::Base.send :after_filter, :add_google_analytics_code
 ActionView::Base.send :include, Rubaidh::GoogleAnalyticsViewHelper
 
-ActionController::AbstractRequest.send :include, Rubaidh::GoogleAnalyticsAccountSupport
+ActionController::Request.send :include, Rubaidh::GoogleAnalyticsAccountSupport
