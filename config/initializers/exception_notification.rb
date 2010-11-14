9c196d65a20b09b839515ca420d518d5109fac84
Alban Peignier <alban@tryphon.eu>
2010-11-14 12:40:13 +0100

    Add a workaround for ExceptionNotification (exception when sending email)

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -3,3 +3,6 @@ ExceptionNotification::Notifier.sender_address = %("Bonnes-Ondes Error" <root@tr
 
 # defaults to "[ERROR] "
 ExceptionNotification::Notifier.email_prefix = "[BonnesOndes] "
+
+# Workaround for https://rails.lighthouseapp.com/projects/8995/tickets/85-exception_notification-2330-fails-with-rails-235
+ExceptionNotification::Notifier.view_paths = ActionView::Base.process_view_paths(ExceptionNotification::Notifier.view_paths)
