3f58f01063bff39c34f209d57bda50e2b9aed7ff
Alban Peignier <alban@tryphon.eu>
2011-01-15 18:49:46 +0100

    Use a constant directory for attachmentfu. Fixes #18

--- config/initializers/attachmentfu_config.rb
+++ config/initializers/attachmentfu_config.rb
@@ -1,16 +1,9 @@
 module Technoweenie # :nodoc:
   module AttachmentFu # :nodoc:
     def self.create_tempfile_path
-      tmp_file = Tempfile.new('attachmentfu', '/tmp')
-      tmp_directory = tmp_file.path
-      tmp_file.delete
-      Dir.mkdir(tmp_directory, 0750)
-
-      Signal.trap("EXIT") do
-        FileUtils.rm_rf tmp_directory
+      "/tmp/bonnesondes-#{Process.uid}".tap do |directory|
+        Dir.mkdir(directory, 0750) unless File.directory?(directory)
       end
-
-      tmp_directory
     end
   end
 end
